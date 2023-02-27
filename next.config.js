6b14325227fe4fba4677afa9f59abefb6b0f51f9
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2023-02-27 16:35:40 -0500

    Fix next.config.js issue, remove unused image config

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  images: {
-    dangerouslyAllowSVG: true,
-    contentDispositionType: "attachment",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
 };
 
 module.exports = nextConfig;
